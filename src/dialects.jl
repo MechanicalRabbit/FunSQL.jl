b0072c712d75f59eb105bb78bdfa644ec8311c8c
Kirill Simonov <xi@resolvent.net>
2021-04-19 11:04:11 -0500

    add SQLDialect.has_window_clause

--- src/dialects.jl
+++ src/dialects.jl
@@ -3,9 +3,28 @@
 """
 Properties of a SQL dialect.
 """
-struct SQLDialect
-    name::Symbol
+@Base.kwdef struct SQLDialect
+    name::Symbol = :default
+    has_window_clause::Bool = false
 end
+
+SQLDialect(name::Symbol) =
+    if name === :postgresql
+        SQLDialect(name = name, has_window_clause = true)
+    elseif name === :sqlite
+        SQLDialect(name = name, has_window_clause = true)
+    elseif name === :mysql
+        SQLDialect(name = name, has_window_clause = true)
+    elseif name === :redshift
+        SQLDialect(name = name)
+    elseif name === :sqlserver
+        SQLDialect(name = name)
+    else
+        SQLDialect()
+    end
+
+Base.show(io::IO, dialect::SQLDialect) =
+    print(io, "SQLDialect(name = $(QuoteNode(dialect.name)), has_window_clause = $(dialect.has_window_clause))")
 
 Base.convert(::Type{SQLDialect}, name::Symbol) =
     SQLDialect(name)
